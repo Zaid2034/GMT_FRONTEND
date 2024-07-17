48cd790e8c3a45f69fa2992f1c58c3c7c67219ed
zaid <mdzaidali2034@gmail.com>
2024-07-17 14:35:15 +0530

    Feature:Added arrow in Splash Screen

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -43,18 +43,42 @@ const SplashScreen = () => {
                         </div>
                     </div>
                     {count<=2?(
-                        <div className="flex justify-between text-white">
+                        <div className="flex justify-between text-white mx-2 font-semibold">
                     
-                        <p className='cursor-pointer' onClick={()=>{
+                        <p className='cursor-pointer text-sm' onClick={()=>{
                             if(count>1){
                                 setCount(count-1)
                             }
                         }}>Skip</p>
-                        <p className='cursor-pointer' onClick={()=>{
-                            if(count<3){
-                                setCount(count+1)}
-                            }
-                        }>Next</p>
+                        <div className='flex justify-center items-center gap-x-1'>
+                            <p
+                            className="cursor-pointer text-sm pb-0.5 font-semibold"
+                            onClick={() => {
+                                if (count < 3) {
+                                setCount (count + 1);
+                                }
+                            }}
+                            >
+                            Next
+                            </p>
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                >
+                                <path
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                    d="M12.2929 5.29289C12.6834 4.90237 13.3166 4.90237 13.7071 5.29289L17.7071 9.29289C18.0976 9.68342 18.0976 10.3166 17.7071 10.7071L13.7071 14.7071C13.3166 15.0976 12.6834 15.0976 12.2929 14.7071C11.9024 14.3166 11.9024 13.6834 12.2929 13.2929L14.5858 11H3C2.44772 11 2 10.5523 2 10C2 9.44772 2.44772 9 3 9H14.5858L12.2929 6.70711C11.9024 6.31658 11.9024 5.68342 12.2929 5.29289Z"
+                                    fill="white"
+                                />
+                            </svg>
+
+
+                        </div>
+                        
                     </div>
 
                     ):(<div className='flex justify-center items-center mb-4'>
